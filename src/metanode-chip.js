bb633e21adc5e1b4ee492a7c294f15e917b17865
Vincent Rubinetti <vince.rubinetti@gmail.com>
2019-04-04 12:42:55 -0400

    change metanode chips to svg
    doing it with text was causing pretty big rendering differences between browsers. letters would be slightly off in different directions. did this with the hope that it would be exactly the same on all browsers, and that the letters would be mathematically center

--- src/metanode-chip.js
+++ src/metanode-chip.js
@@ -3,9 +3,22 @@ import { Component } from 'react';
 
 import { Metatypes } from './metatypes.js';
 import './metanode-chip.css';
-import { ReactComponent as IconArrowBoth } from './arrow-icon-both.svg';
-import { ReactComponent as IconArrowForward } from './arrow-icon-forward.svg';
-import { ReactComponent as IconArrowBackward } from './arrow-icon-backward.svg';
+
+import { ReactComponent as ArrowBoth } from './arrow-icon-both.svg';
+import { ReactComponent as ArrowForward } from './arrow-icon-forward.svg';
+import { ReactComponent as ArrowBackward } from './arrow-icon-backward.svg';
+
+import { ReactComponent as Gene } from './chip-gene.svg';
+import { ReactComponent as Compound } from './chip-compound.svg';
+import { ReactComponent as Anatomy } from './chip-anatomy.svg';
+import { ReactComponent as Disease } from './chip-disease.svg';
+import { ReactComponent as Symptom } from './chip-symptom.svg';
+import { ReactComponent as SideEffect } from './chip-side-effect.svg';
+import { ReactComponent as BiologicalProcess } from './chip-biological-process.svg';
+import { ReactComponent as CellularComponent } from './chip-cellular-component.svg';
+import { ReactComponent as MolecularFunction } from './chip-molecular-function.svg';
+import { ReactComponent as Pathway } from './chip-pathway.svg';
+import { ReactComponent as PharmacologicClass } from './chip-pharmacologic-class.svg';
 
 // metanode 'chip' component
 // colored circle with abbreviation text in middle
@@ -14,16 +27,46 @@ export class MetanodeChip extends Component {
   // display component
   render() {
     const node = Metatypes.lookup(this.props.type);
-    return (
-      <div
-        className={
-          'metanode_chip ' + (node.abbreviation.length > 1 ? 'small' : '')
-        }
-        data-name={node.name}
-        data-abbreviation={node.abbreviation}
-        style={{ backgroundColor: node.backgroundColor, color: node.textColor }}
-      />
-    );
+
+    // get icon
+    let icon;
+    switch (node.name) {
+      case 'Gene':
+        icon = <Gene />;
+        break;
+      case 'Compound':
+        icon = <Compound />;
+        break;
+      case 'Anatomy':
+        icon = <Anatomy />;
+        break;
+      case 'Disease':
+        icon = <Disease />;
+        break;
+      case 'Symptom':
+        icon = <Symptom />;
+        break;
+      case 'Side Effect':
+        icon = <SideEffect />;
+        break;
+      case 'Biological Process':
+        icon = <BiologicalProcess />;
+        break;
+      case 'Cellular Component':
+        icon = <CellularComponent />;
+        break;
+      case 'Molecular Function':
+        icon = <MolecularFunction />;
+        break;
+      case 'Pathway':
+        icon = <Pathway />;
+        break;
+      case 'Pharmacologic Class':
+        icon = <PharmacologicClass />;
+        break;
+    }
+
+    return <div className='metanode_chip'>{icon}</div>;
   }
 }
 
@@ -33,29 +76,27 @@ export class MetaedgeChip extends Component {
   // display component
   render() {
     // get edge direction icon
-    let directionIcon;
+    let icon;
     switch (this.props.direction) {
       case 'backward':
-        directionIcon = <IconArrowBackward />;
+        icon = <ArrowBackward />;
         break;
       case 'forward':
-        directionIcon = <IconArrowForward />;
+        icon = <ArrowForward />;
         break;
       default:
-        directionIcon = <IconArrowBoth />;
+        icon = <ArrowBoth />;
         break;
     }
 
     const node = Metatypes.lookup(this.props.type);
     return (
       <div
-        className={
-          'metaedge_chip ' + (node.abbreviation.length > 1 ? 'small' : '')
-        }
+        className='metaedge_chip'
         data-name={node.name}
         data-abbreviation={node.abbreviation}
       >
-        {directionIcon}
+        {icon}
       </div>
     );
   }
