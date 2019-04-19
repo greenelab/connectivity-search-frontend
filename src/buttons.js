4963e00e731ca6aba26c7377e220c29fa551c7f4
Vincent Rubinetti <vince.rubinetti@gmail.com>
2019-04-19 12:28:22 -0400

    remove unused var, fix ci build fail

--- src/buttons.js
+++ src/buttons.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 import { Tooltip } from './tooltip.js';
 import './buttons.css';
