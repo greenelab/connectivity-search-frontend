greenelab/connectivity-search-frontend

commit 224a39b79158199374e43cdc8d581c34ef4cdeae

Backend query cache (#77)
- automatically caches any queries to the backend in sessionStorage, and uses the cached response when the same query is made
- changes the null/undefined/falsey behavior in the master reducer
- doubles the timeout for the app render test, to hopefully prevent circleci tests from failing so often due to simple timeout
- adds error message for server non-successful response
- clicking the link to the neo4j browser in the metapath table no longer also sorts that column
- fixes an app-crashing glitch where path-query returns no paths sub-object

src/path-graph.js
@@ -953,7 +953,12 @@ export class Graph extends Component {
       pathQueries = this.props.pathQueries;
 
     // if pathQueries not valid, exit
-    if (!pathQueries || pathQueries.length <= 0)
+    if (
+      !pathQueries ||
+      pathQueries.length <= 0 ||
+      !pathQueries.paths ||
+      pathQueries.paths.length <= 0
+    )
       return graph;
 
     // get source/target nodes from first path in pathQueries

src/actions.js
@@ -40,11 +40,7 @@ export function swapSourceTargetNodes() {
 }
 
 // update metapaths
-export function updateMetapaths({
-  metapaths,
-  dontUpdateUrl,
-  preserveChecks
-}) {
+export function updateMetapaths({ metapaths, dontUpdateUrl, preserveChecks }) {
   return {
     type: 'update_metapaths',
     payload: { metapaths: metapaths },

src/app.test.js
@@ -20,4 +20,4 @@ test('renders without crashing', async () => {
   );
   expect(children).toBeGreaterThan(0);
   browser.close();
-}, 20000);
+}, 40000);

src/backend-query.js
@@ -22,16 +22,37 @@ const metapathSearchServer = 'https://search-api.het.io/v1/query-metapaths/';
 const pathSearchServer = 'https://search-api.het.io/v1/query-paths/';
 
 // get resource at url and parse as json
-export function fetchJson(url) {
-  return fetch(url)
-    .then((response) => response.json())
-    .then((results) => {
-      return results || {};
-    })
-    .catch((error) => {
-      console.log(error, url);
-      return {};
-    });
+export function fetchJson(url, dontCache) {
+  // check if query has already been made during this session
+  // if so, use cache of that. if not, query server
+  const cachedResponse = window.sessionStorage.getItem(url);
+  if (cachedResponse && !dontCache)
+    return Promise.resolve(JSON.parse(cachedResponse));
+  else {
+    return fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          console.log('response', response.status);
+          return {};
+        } else
+          return response.json();
+      })
+      .then((results) => {
+        if (!dontCache) {
+          try {
+            // save response to cache. use try/catch in case storage
+            window.sessionStorage.setItem(url, JSON.stringify(results));
+          } catch (error) {
+            console.log(error, url);
+          }
+        }
+        return results || {};
+      })
+      .catch((error) => {
+        console.log(error, url);
+        return {};
+      });
+  }
 }
 
 // get metagraph
@@ -51,7 +72,7 @@ export function getHetioStyles() {
 
 // get hetmech definitions
 export function getHetmechDefinitions() {
-  return hetmechDefinitions;
+  return Promise.resolve(hetmechDefinitions);
 }
 
 // lookup node by id
@@ -89,7 +110,7 @@ export function searchNodesMetapaths(otherNode) {
 // get random source/target node pair that has metapath(s)
 export function getRandomNodePair() {
   const query = randomNodeServer;
-  return fetchJson(query).then((response) => {
+  return fetchJson(query, true).then((response) => {
     return response;
   });
 }

src/index.js
@@ -8,6 +8,9 @@ import { Provider } from 'react-redux';
 import { Reducer } from './reducers.js';
 import { App } from './app.js';
 
+// clear cache when app first starts (page refresh)
+window.sessionStorage.clear();
+
 // create global state store
 const store = createStore(Reducer, applyMiddleware(thunk));
 

src/metapath-results.js
@@ -35,8 +35,7 @@ export class MetapathResults extends Component {
       <section>
         <CollapsibleSection
           label='Metapaths'
-          tooltipText=
-            'Metapaths of length <= 3 between the source and target node'
+          tooltipText='Metapaths of length <= 3 between the source and target node'
         >
           {this.props.metapaths.length > 0 ? <TableFull /> : <TableEmpty />}
         </CollapsibleSection>
@@ -440,7 +439,9 @@ class TableHead extends Component {
             <a
               href='https://neo4j.het.io/browser/'
               target='_blank'
-              rel='noopener noreferrer'>
+              rel='noopener noreferrer'
+              onClick={(event) => event.stopPropagation()}
+            >
               neo4j query
             </a>
           }

src/reducers.js
@@ -15,63 +15,52 @@ export function Reducer(prevState, action) {
   switch (action.type) {
     // set definitions
     case 'set_definitions':
-      if (action.payload.metagraph !== undefined)
-        newState.metagraph = copyObject(action.payload.metagraph);
-      if (action.payload.hetioDefinitions !== undefined)
-        newState.hetioDefinitions = copyObject(action.payload.hetioDefinitions);
-      if (action.payload.hetioStyles !== undefined)
-        newState.hetioStyles = copyObject(action.payload.hetioStyles);
-      if (action.payload.hetmechDefinitions !== undefined) {
-        newState.hetmechDefinitions = copyObject(
-          action.payload.hetmechDefinitions
-        );
-      }
+      newState.metagraph = copyObject(action.payload.metagraph) || {};
+      newState.hetioDefinitions =
+        copyObject(action.payload.hetioDefinitions) || {};
+      newState.hetioStyles = copyObject(action.payload.hetioStyles) || {};
+      newState.hetmechDefinitions =
+        copyObject(action.payload.hetmechDefinitions) || {};
       break;
 
     // update source and/or target node
     case 'update_source_target_nodes':
       if (action.payload.sourceNode !== undefined)
-        newState.sourceNode = copyObject(action.payload.sourceNode);
+        newState.sourceNode = copyObject(action.payload.sourceNode) || {};
       if (action.payload.targetNode !== undefined)
-        newState.targetNode = copyObject(action.payload.targetNode);
+        newState.targetNode = copyObject(action.payload.targetNode) || {};
       break;
 
     // swap source/target nodes
     case 'swap_source_target_nodes':
-      if (prevState.sourceNode && prevState.targetNode) {
-        newState.sourceNode = copyObject(prevState.targetNode);
-        newState.targetNode = copyObject(prevState.sourceNode);
-      }
+      newState.sourceNode = copyObject(prevState.targetNode) || {};
+      newState.targetNode = copyObject(prevState.sourceNode) || {};
       break;
 
     // update metapaths
     case 'update_metapaths':
-      if (action.payload.metapaths !== undefined) {
-        newState.metapaths = copyObject(action.payload.metapaths);
-        if (action.preserveChecks === true) {
-          transferObjectProps(
-            prevState.metapaths,
-            newState.metapaths,
-            ['id'],
-            ['checked']
-          );
-        }
+      newState.metapaths = copyObject(action.payload.metapaths) || [];
+      if (action.preserveChecks === true) {
+        transferObjectProps(
+          prevState.metapaths,
+          newState.metapaths,
+          ['id'],
+          ['checked']
+        );
       }
       break;
 
     // update path queries
     case 'update_path_queries':
-      if (action.payload.pathQueries !== undefined) {
-        newState.pathQueries = copyObject(action.payload.pathQueries);
-        if (action.preserveChecks === true) {
-          transferQueryProps(
-            prevState.pathQueries,
-            newState.pathQueries,
-            'paths',
-            ['node_ids', 'rel_ids'],
-            ['checked', 'highlighted']
-          );
-        }
+      newState.pathQueries = copyObject(action.payload.pathQueries) || [];
+      if (action.preserveChecks === true) {
+        transferQueryProps(
+          prevState.pathQueries,
+          newState.pathQueries,
+          'paths',
+          ['node_ids', 'rel_ids'],
+          ['checked', 'highlighted']
+        );
       }
       break;
 

src/util.js
@@ -286,5 +286,8 @@ export function cutString(string, n) {
 // make deep copy of object. ensures everything is clone/copy, not reference.
 // works for everything except circular refs, functions, and js Dates
 export function copyObject(object) {
-  return JSON.parse(JSON.stringify(object));
+  if (object === undefined)
+    return;
+  else
+    return JSON.parse(JSON.stringify(object));
 }

commit 66dab51ce1a7e3fa87ea99dc0ac5f561aff9a229

fix bug where path graph never shows (#80)
- fixes a regression from PR #77 where the path graph shows nothing even though there is valid node/edge data to show

src/path-graph.js
@@ -956,8 +956,8 @@ export class Graph extends Component {
     if (
       !pathQueries ||
       pathQueries.length <= 0 ||
-      !pathQueries.paths ||
-      pathQueries.paths.length <= 0
+      !pathQueries[0].paths ||
+      pathQueries[0].paths.length <= 0
     )
       return graph;
 