06c2cdf896b5718de865bc582fae584872f2d87f
Vincent Rubinetti <vince.rubinetti@gmail.com>
2019-08-29 15:21:04 -0400

    move download function back to hetio-components

--- src/path-graph/download.js
+++ src/path-graph/download.js
@@ -1,16 +1,14 @@
 import { makeFilenameFriendly } from 'hetio-frontend-components';
-// import { downloadSvg } from 'hetio-frontend-components';
+import { downloadSvg } from 'hetio-frontend-components';
 
 import decode from 'unescape';
 
 // download path graph
 export function download(sourceNode, targetNode) {
-  if (!sourceNode.name || !targetNode.name)
-    return;
+  if (!sourceNode.name || !targetNode.name) return;
 
   let svg = document.getElementById('graph');
-  if (!svg)
-    return;
+  if (!svg) return;
 
   svg = decode(svg.outerHTML);
 
@@ -21,17 +19,4 @@ export function download(sourceNode, targetNode) {
   ].join('_');
 
   downloadSvg(svg, filename);
-}
-
-// downloads provided data as svg file
-function downloadSvg(fileContents, filename) {
-  const blob = new Blob([fileContents], { type: 'image/svg+xml' });
-  const url = window.URL.createObjectURL(blob);
-  const link = document.createElement('a');
-  document.body.appendChild(link);
-  link.href = url;
-  link.download = (filename || 'data') + '.svg';
-  link.click();
-  window.URL.revokeObjectURL(url);
-  link.remove();
 }
